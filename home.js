c4bac4a25d4ad1243526bc01ba4b927e66f552db
TheMonster155 <136197595+TheMonster155@users.noreply.github.com>
2024-07-03 18:50:46 +0200

    Add files via upload

--- home.js
+++ home.js
@@ -20,35 +20,56 @@ const navBarLinks = [
 const nav = document.getElementById("nav_links");
 
 const createNavbarLink = (singleLink) => {
-    const li = document.createElement("li");
-    li.classList.add("nav-item");
-    const a = document.createElement("a");
-    a.innerText = singleLink.title
-    a.href = singleLink.href
-    a.classList.add("nav-link");
-    li.appendChild(a);
-    nav.appendChild(li);
-}
+  const li = document.createElement("li");
+  li.classList.add("nav-item");
+  const a = document.createElement("a");
+  a.innerText = singleLink.title;
+  a.href = singleLink.href;
+  a.classList.add("nav-link");
+  li.appendChild(a);
+  nav.appendChild(li);
+};
+
+navBarLinks.forEach((navBarLink) => {
+  createNavbarLink(navBarLink);
+});
+
+const skills = [
+  { text: "30,000 online courses ", subtitle: "enjoy variety of fresh topics" },
+  {
+    text: "Expert instruction  ",
+    subtitle: "find the right instruction for you ",
+  },
+  { text: "Lifetime access  ", subtitle: "learn on your schedule" },
+];
+
+const skillsContainer = document.getElementById("skills");
+
+const createSkills = (singolSkill) => {
+  const div = document.createElement("div");
+  div.setAttribute("class", "col-4 skills");
+  const divRow = document.createElement("div");
+  divRow.setAttribute("class", "row ");
+  const divColIntern = document.createElement("div");
+  divColIntern.setAttribute(
+    "class",
+    "col-4  centered-text mx-auto "
+  );
+  const h3 = document.createElement("h3");
+  h3.innerText = singolSkill.text;
+  const p = document.createElement("p");
+  p.setAttribute("class", "text-secondary");
+  p.innerText = singolSkill.subtitle;
+
+  div.appendChild(divRow);
+  divRow.appendChild(divColIntern);
+  divColIntern.append(h3, p);
+  skillsContainer.appendChild(div);
+};
 
 
-navBarLinks.forEach(navBarLink => {
-    createNavbarLink(navBarLink)
+skills.forEach((singolSkills) => {
+  createSkills(singolSkills);
 });
 
 
-
-const skills = [
-    { text: "30,000 online colses ",  subtitle: "enjois varitis off fresh topics"  },
-    { text: "exspert instrucion  ",  subtitle: "find the rais insctution for you "  },
-    { text: "life time access  ",  subtitle: "learn on your scable"  },
-
-]
-
-
-const skillsContainer = document.getElementById("skills")
-
-const createSkills  = ( singolSkill ) => {
-    const div = document.createElement("div");
-    div.classList.add("col-4")
-    
-}
